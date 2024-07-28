a3ffadabb847e387dee8b0732e43167cbc7cd305
unknown <olivia.bergaglia@gmail.com>
2024-07-28 02:16:14 +0200

    New content and scss

--- js/about.js
+++ js/about.js
@@ -1,8 +1,6 @@
 export function displayAbout() {
     document.getElementById("title").innerHTML = `<h1>About</h1>`
-    document.getElementById("text").innerHTML = `<p>I'm Olivia, and I'm 26 years old. I'm now studying development, primarily focusing on web development, although I have a strong interest in low-level programming.</p>
-                                                <p>I enjoy studying many aspects of computer science and coding in my own time. I also like to play sports. Apart from that, I have a strong affinity for electronic music and graphic design. I also enjoy organizing and attending a variety of events, which is another reason why I have a keen interest in event planning.</p>
-                                                <p>I can blend technical know-how with creativity because to my wide range of interests, and I'm always eager to learn new things and extend my horizons. I strive to give everything I do my all, whether it's building a useful piece of software or designing a visually beautiful website.</p>`
+    document.getElementById("text").innerHTML = ``
 }
 
 
